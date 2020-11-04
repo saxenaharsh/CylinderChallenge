98dc231d8580428317ed5d3cff519eb672ae754c
saxenaharsh <harsh.saxena@gmail.com>
2020-11-04 21:17:48 +0530

    Add files via upload

---
+++ CylinderChallenge/src/com/HarshSaxena/Main.java
@@ -0,0 +1,20 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Circle circle = new Circle(3.75);
+        System.out.println("Circle radius = " + circle.getRadius());
+        System.out.println("Circle area = " + circle.getArea());
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+        System.out.println("cylinder radius = " + cylinder.getRadius());
+        System.out.println("cylinder height = " + cylinder.getHeight());
+        System.out.println("cylinder area = " + cylinder.getArea());
+        System.out.println("cylinder Volume = " + cylinder.getVolume());
+
+
+
+    }
+}
